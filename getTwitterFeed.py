2f3a79924be1efaf128df9936fbd6bf579918bd3
asood <atinsood@gmail.com>
2013-03-16 14:08:37 -0400

    successfuly working twitter feed

--- getTwitterFeed.py
+++ getTwitterFeed.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 
 import urllib2
+import json
 
 def getTwitterSearchResults(hashtags=['galaxy', 'samsung', 's4']):
     response = urllib2.urlopen('http://search.twitter.com/search.json?q=%23samsung%20OR%20%23galaxys4%20lang%3Aen&src=typd')
-    print response.read()
+    jsonResponse = json.loads(response.read())
+    return jsonResponse['results']
 
+
+def printResutsToFile(result):
+    myFile = open('/Users/asood/work/opensource/play/bostonHackathon/hackathon/twitter.txt', 'w')
+    for item in result:
+        myFile.write(item['text'])
+        myFile.write("\n")
+    myFile.close() 
 
 if __name__ == "__main__":
-    getTwitterSearchResults()
-
+    twitterResult = getTwitterSearchResults()
+    printResutsToFile(twitterResult)
