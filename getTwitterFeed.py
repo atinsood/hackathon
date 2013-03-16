7ecc7894cebdbd1bbadb12620d633a666b0dc286
asood <atinsood@gmail.com>
2013-03-16 14:34:42 -0400

    successfuly working twitter feed

--- getTwitterFeed.py
+++ getTwitterFeed.py
@@ -3,8 +3,8 @@
 import urllib2
 import json
 
-def getTwitterSearchResults(hashtags=['galaxy', 'samsung', 's4']):
-    response = urllib2.urlopen('http://search.twitter.com/search.json?q=%23samsung%20OR%20%23galaxys4%20lang%3Aen&src=typd')
+def getTwitterSearchResults(url='http://search.twitter.com/search.json?q=%23samsung%20OR%20%23galaxys4%20lang%3Aen&src=typd&rpp=100&page=1', hashtags=['galaxy', 'samsung', 's4']):
+    response = urllib2.urlopen(url)
     jsonResponse = json.loads(response.read())
     return jsonResponse['results']
 
@@ -12,10 +12,13 @@ def getTwitterSearchResults(hashtags=['galaxy', 'samsung', 's4']):
 def printResutsToFile(result):
     myFile = open('/Users/asood/work/opensource/play/bostonHackathon/hackathon/twitter.txt', 'w')
     for item in result:
-        myFile.write(item['text'])
+        myFile.write(item['text'].encode('ascii', errors='ignore'))
         myFile.write("\n")
     myFile.close() 
 
 if __name__ == "__main__":
-    twitterResult = getTwitterSearchResults()
-    printResutsToFile(twitterResult)
+    url = 'http://search.twitter.com/search.json?q=%23samsung%20OR%20%23galaxys4%20lang%3Aen&src=typd&rpp=100&page='
+    for i in range(1,10):
+        query = url + str(i)
+        twitterResult = getTwitterSearchResults(query)
+        printResutsToFile(twitterResult)
